9216329f9b742e540a5d07cb2fdfcb58baae8397
Noah Tyree <noahjtyree@gmail.com>
2024-01-29 18:44:17 -0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double cardBalance = 5000;
+        double monthOneBalance;
+        double monthTwoBalance;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        monthOneBalance = cardBalance * 1.17;
+        monthTwoBalance = monthOneBalance * 1.17;
+
+        System.out.println("Your interest after one month is " +monthOneBalance);
+        System.out.println("Your interest after two months is " +monthTwoBalance);
+
     }
 }
